a1bd455078066d9841f08144927375a87e29dfb0
friche11 <vicfriche@gmail.com>
2024-03-01 14:51:29 -0300

    Arquivos bootstrap

--- SpringWeb/src/main/java/com/example/friche/SpringWeb/controllers/HomeController.java
+++ SpringWeb/src/main/java/com/example/friche/SpringWeb/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.friche.SpringWeb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +9,9 @@ public class HomeController {
 
     //Rota para home page
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("nome", "Victor");
         return "home/index";
     }
 
