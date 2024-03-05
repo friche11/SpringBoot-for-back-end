e38ace36b6464e48e3b1402cc98b67a14312a8dd
friche11 <vicfriche@gmail.com>
2024-03-04 23:58:12 -0300

    Criar e excluir admin

--- SpringWeb/src/main/java/com/example/friche/SpringWeb/controllers/AdministradoresController.java
+++ SpringWeb/src/main/java/com/example/friche/SpringWeb/controllers/AdministradoresController.java
@@ -1,15 +1,50 @@
 package com.example.friche.SpringWeb.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.friche.SpringWeb.models.Administrador;
+import com.example.friche.SpringWeb.repositorio.AdministradoresRepo;
+
 
 @Controller
 public class AdministradoresController {
 
-    //Rota para home page
+    @Autowired
+    private AdministradoresRepo repo;
+
+    //Rota para página de admin
     @GetMapping("/administradores")
-    public String index(){
+    public String index(Model model){
+        List<Administrador> administradores = (List<Administrador>) repo.findAll();
+        model.addAttribute("administradores", administradores);
         return "administradores/index";
+    }
+
+    //Rota para página de cadastro de admin
+    @GetMapping("/administradores/novo")
+    public String novo(){
+        return "administradores/novo";
+    }
+
+    //Rota para metodo POST de cadastro
+    @PostMapping("/administradores/criar")
+    public String criar(Administrador administrador){
+        repo.save(administrador);
+        return "redirect:/administradores";
+    }
+
+    //Rota para excluir cadastro
+    @GetMapping("/administradores/{id}/excluir")
+    public String excluir(@PathVariable int id){
+        repo.deleteById(id);
+        return "redirect:/administradores";
     }
 
 }
