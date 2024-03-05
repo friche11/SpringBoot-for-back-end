0fd571e6f39e098a5155ce2c9a226b2e58b872b3
friche11 <vicfriche@gmail.com>
2024-03-04 22:19:17 -0300

    Modelo de administrador

---
+++ SpringWeb/src/main/java/com/example/friche/SpringWeb/models/Administrador.java
@@ -0,0 +1,55 @@
+package com.example.friche.SpringWeb.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "administradores")
+public class Administrador {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email",  nullable = false)
+    private String email;
+
+    @Column(name = "senha",  nullable = false)
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
---
+++ SpringWeb/src/main/java/com/example/friche/SpringWeb/repositorio/AdministradoresRepo.java
@@ -0,0 +1,9 @@
+package com.example.friche.SpringWeb.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.friche.SpringWeb.models.Administrador;
+
+public interface AdministradoresRepo extends CrudRepository<Administrador, Integer>{
+
+}
